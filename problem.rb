05e8caeb7a0c8a2094f8473a64abf62bf7988da4
Nikhat Bhayani <nikhataly@gmail.com>
2015-06-16 16:58:25 -0400

    Works

--- problem.rb
+++ problem.rb
@@ -5,8 +5,8 @@ def convert_string(input)
   length = my_array.length
   s = ""
   counter = length
-  until counter < 0
-    s += n
+  until counter == 0
+    s += my_array[counter].to_s
     counter = counter - 1
   end
   puts s
