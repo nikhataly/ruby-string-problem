2936afe272afd3c0002ef03e2ab5005c6f5808e6
Nikhat Bhayani <nikhataly@gmail.com>
2015-06-16 16:52:13 -0400

    Trying

--- problem.rb
+++ problem.rb
@@ -1,8 +1,15 @@
 # Reverse a string without the .reverse method
 
 def convert_string(input)
-  puts "method call works"
+  my_array = input.split("")
+  length = my_array.length
+  s = ""
+  counter = length
+  until counter < 0
+    s += n
+    counter = counter - 1
+  end
+  puts s
 end
 
-convert_string("apple")
-
+convert_string("apple, bees")
